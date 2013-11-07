87871e71f4bccf77956795b3c1a411cfb1cf390f
robthompsonweb <robt_work@yahoo.co.uk>
2013-11-07 13:48:49 +1100

    Parsing inputs

--- classes/recipe-finder.class.php
+++ classes/recipe-finder.class.php
@@ -1,16 +1,29 @@
 <?php
 class RecipeFinder {
     
+    //List of ingredients in the fridge
     private $fridgeListCsv = null;
+    private $fridgeList = array();
+    
+    //List of recipes
     private $recipesJson = null;
+    private $recipes = array();
+    
+    //Holds any error message
+    private $errorStr = '';
     
     function __construct($fridgeList, $recipes) {
         $this->fridgeListCsv = $fridgeList;
         $this->recipesJson = $recipes;
     }
     
-    public function findRecipe() {
-        return 'Salad';
+    private function _parseFridgeList() {
+        
     }
+    
+    public function getError() {
+        return $this->errorStr;
+    }
+    
 }
 ?>
